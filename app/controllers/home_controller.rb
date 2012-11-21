3561ff8c38a410dc82dd301e581aaffc8fb88cc4
alicechih <alicechih8096@gmail.com>
2012-11-21 18:11:58 +0800

    search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,24 +7,6 @@ class HomeController < ApplicationController
     #@res = @db.query("select * from `patent_2007` where `Patent_id` = '"+params[:test]+"'")
   end
 
-  def searchPID
-  	#render :text => "OK!!!!!!!!!!!!"
-    @patent = @db.query("select * from `patent_2007` where `Patent_id` = '"+params[:patentID]+"'")
-    @inventor = @db.query("select * from `inventor_2007` where `Patent_id` = '"+params[:patentID]+"'")
-    @assignee = @db.query("select * from `assignee_2007` where `Patent_id` = '"+params[:patentID]+"'")
-    #render :inline => "<% @patent.each do |r| %><%= r.to_s %><% end %><br><% @inventor.each do |r| %><%= r.to_s %><% end %><br><% @assignee.each do |r| %><%= r.to_s %><% end %>"
-  end
-
-def searchINV
-    @patentID = @db.query("select `Patent_id` from `inventor_2007` where `Name` like '%"+params[:INV]+"%'")
-    #@array = Array.new
-    #@patentID.each do |p|
-    #  params[:patent] = @db.query("select `Patent_id`, `Abstract` from `patent_2007` where `Patent_id` = '"+p['Patent_id']+"'")
-    #  @array << params[:patent].to_a[0]
-    #end
-    #render :inline => "<% @array.each do |a| %><%= a.to_s %><% end %>"
-  end
-
   def searchASS
     @assignee_name = params[:companyIn]
     @patent = Array.new
@@ -78,22 +60,15 @@ def IPCPieChart
   end
 
   if @layer == 0
-    #@patentID = @db.query("select `Patent_id` from `ipc_2007` limit 0,8")
-    @patentID = @db.query("select `Patent_id` from `assignee_2007` where `Assignee` like '%Kohler%' limit 0,7")
-    #@patentID = @db.query("select `Patent_id` from `assignee_2007` where `Assignee` like '%"+params[:ASS]+"%'")
-    @array = Array.new
-    
-    @patentID.each do |p|
-      params[:patent] = @db.query("select * from `ipc_2007` where `Patent_id` = '"+p['Patent_id']+"'")
-      $k = params[:patent].to_a.length
-      for i in 0..$k-1
-        if params[:patent].to_a[i]['IPC_class'][0] =~ /\D/
-          @array.push( params[:patent].to_a[i] )
-        end
-      end
-    end
-
-    @arraySorted = @array.sort! { |x,y| x["IPC_class"] <=> y["IPC_class"] }
+    @assignee_name = "Kohler"
+    @iPC_data = @db.query("SELECT  `Patent_id`, `IPC_class`
+                               FROM  `assignee_2007` 
+                               LEFT JOIN  `ipc_2007` 
+                               USING ( Patent_id ) 
+                               WHERE assignee_2007.Assignee
+                               REGEXP  '#{@assignee_name}'
+                               ORDER BY 2 ASC")
+    @arraySorted = @iPC_data.to_a
 
     @array_hash = Array.new
     for j in 0..4
@@ -104,27 +79,30 @@ def IPCPieChart
     
     for i in 0..$k-1
       # splitting IPC into 5 segments
-      iPCchar_array = Array.new
-      a = @arraySorted[i]["IPC_class"].to_s
-      for n in [0,2,3]
-        iPCchar_array << a[0..n]
-      end
-      iPCchar_array << a.partition(/\//)[0] + "/00"
-      iPCchar_array << a
+      if @arraySorted[i]["IPC_class"].to_s != "" && @arraySorted[i]["IPC_class"].to_s[0] =~ /\D/
+        iPCchar_array = Array.new
+        a = @arraySorted[i]["IPC_class"].to_s
+        for n in [0,2,3]
+          iPCchar_array << a[0..n]
+        end
+        iPCchar_array << a.partition(/\//)[0] + "/00"
+        iPCchar_array << a
 
-      for j in 0..4
-        if @array_hash[j].has_key?(iPCchar_array[j]) == true
-          @array_hash[j][iPCchar_array[j]] += 1
-        else
-          @array_hash[j].store(iPCchar_array[j], 1)
+        for j in 0..4
+          if @array_hash[j].has_key?(iPCchar_array[j]) == true
+            @array_hash[j][iPCchar_array[j]] += 1
+          else
+            @array_hash[j].store(iPCchar_array[j], 1)
+          end
         end
       end
+
     end
     
     @result = @array_hash
 
   else
-    @result = flash[:result]
+    @result = session[:result]
   end
 end
 
