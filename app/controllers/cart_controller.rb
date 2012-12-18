7c4e6c22455e683612c3e54e40c17d3967e3ba40
cytms <cytmos@gmail.com>
2012-12-18 22:40:55 +0800

    cart table

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,32 +1,23 @@
 class CartController < ApplicationController
-  before_filter :connect_db #, :except =>[:index]
+  before_filter :connect_db , :authenticate_user!
   
   def index
-    # [{"patents"=>"[{\"patent_id\"=>\"1\", \"issued_year\"=>\"2007\", \"title\"=>\"hello\", \"assignee\"=>\"NTU\"}]"}] 
-    #@test = Array.new
-    @data = Array.new
-    @data = [{"patent_id"=>"1", "issued_year"=>"2007", "title"=>"hello", "assignee"=>"NTU"}]
-    #@test = data[0]["patent_id"].to_s
-    @query = @db.query("select `patents` from `carts` limit 1, 1").to_a.first
-    @tmp = @query["patents"].to_s.chomp("\\")
-    @test = JSON.parse(@tmp)
-    @test.each{|patent|
-      patent.each{|x, y|}
-      puts "============="
-      puts patent.keys
-      puts "============="
-    }
-    #s@test2 = JSON.parse(@test).to_s.chomp("\\")
-    #@test = JSON.parse(@query)
+    @query = @db.query("select * from `carts` where `user_id`=#{current_user.id}").to_a.first
+    if @query["patents"].nil? == true then
+      @result_patents = nil
+    else
+      @tmp = @query["patents"].to_s.chomp("\\")
+      @result_patents = JSON.parse(@tmp)
+    end
 
-    #puts "========================"
-    #@test2 = @test[0]["patents"].to_s.chomp("\\")
-    #@test3 = Array.new
-    #@test3 = @test2
-    #puts "test3[0] = " + @test3[0]
-    #puts @test2
-    #puts "========================"
-
+    #group
+    @query = @db.query("select * from `carts` where `user_id`=#{current_user.id}").to_a.first
+    if @query["groups"].nil? == true then
+      @result_groups = nil
+    else
+      @tmp = @query["groups"].to_s.chomp("\\")
+      @result_groups = JSON.parse(@tmp)
+    end
   end
 
   def addPatent
